7f1529ae1d079b3bdceaf0f688313c7afc4a38ec
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-05 07:13:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -54,12 +54,6 @@ class Triangle
       return false
     end
     
-   if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
   end
   
   class TriangleError < StandardError
