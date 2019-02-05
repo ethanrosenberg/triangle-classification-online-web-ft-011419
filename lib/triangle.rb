6dbb8d26eeec311f8c11a5ab6c1f0029d8782d7e
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-05 07:08:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,12 +27,25 @@ class Triangle
     end
  end
  
+ def validtriangle?(one_two_sum, two_three_sum, one_three_sum)
+   if 
+ 
   def correct?
     #binding.pry
     #dirty and long need to refactor
     one_two_sum = @sides[0] + @sides[1]
     two_three_sum = @sides[1] + @sides[2]
     one_three_sum = @sides[0] + @sides[2]
+    
+    count = 0
+    
+    @sides.each do |side|
+      count += side
+    end
+    
+    if count <= 0
+      
+    end
     
    if (@triangle_sides.none? {|side| side <= 0}) &&
       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
